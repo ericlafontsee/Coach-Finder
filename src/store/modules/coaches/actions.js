2d274476433c5f9d949897f03b24ebf405b80018
Eric LaFontsee <ericlafontsee@Erics-MacBook-Pro.local>
2022-10-03 15:39:19 -0500

    added a get request to firebase for all coaches

--- src/store/modules/coaches/actions.js
+++ src/store/modules/coaches/actions.js
@@ -12,16 +12,39 @@ export default {
       `https://coach-app-f9fe6-default-rtdb.firebaseio.com/coaches/${userId}.json`,
       {
         method: 'PUT',
-        body: JSON.stringify(coachData)
+        body: JSON.stringify(coachData),
       }
-    );  
+    );
     // const responseData = await response.json();
-    if(!response.ok){
+    if (!response.ok) {
       //error
     }
     context.commit('registerCoach', {
       ...coachData,
-      id: userId
+      id: userId,
     });
+  },
+  async loadCoaches(context) {
+    const response = await fetch(
+      `https://coach-app-f9fe6-default-rtdb.firebaseio.com/coaches.json`
+    );
+    const resopnseData = await response.json();
+
+    if (!response.ok) {
+      //error
+    }
+    const coaches = [];
+    for (const key in resopnseData) {
+      const coach = {
+        id: key,
+        firstName: resopnseData[key].firstName,
+        lastName: resopnseData[key].lastName,
+        description: resopnseData[key].description,
+        hourlyRate: resopnseData[key].hourlyRate,
+        areas: resopnseData[key].areas,
+      };
+      coaches.push(coach); 
+    }
+    context.commit('setCoaches', coaches);
   },
 };
